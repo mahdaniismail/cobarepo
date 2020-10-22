40121eff3a7bd18c6d725a76c374a1ead04187e3
mahdaniismail <mahdaniismail7@gmail.com>
2020-10-22 14:45:12 +0700

    Ujian Online Ku

---
+++ app/Models/ProductModel.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProductModel extends Model
+{
+    protected $table = "product";
+
+    public function getProduct($id = false)
+    {
+        if ($id === false) {
+            return $this->table('products')
+                ->get()
+                ->getResultArray();
+        } else {
+            return $this->table('products')
+                ->where('product_id', $id)
+                ->get()
+                ->getRowArray();
+        }
+    }
+
+    public function insert_product($data)
+    {
+        return $this->db->table($this->table)->insert($data);
+    }
+
+    public function update_product($data, $id)
+    {
+        return $this->db->table($this->table)->update($data, ['product_id' => $id]);
+    }
+
+    public function delete_product($id)
+    {
+        return $this->db->table($this->table)->delete(['product_id' => $id]);
+    }
+}
---
+++ app/Views/templates/header.php
@@ -0,0 +1,9 @@
+<html>
+
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+
+<body>
+
+    <h1><?= esc($title); ?></h1>
